4731c011067787601427df5478117702ea96f550
Magnolist Mint <ashe.moira@gmail.com>
2018-09-12 01:49:40 +0900

    feat: add buffer sample

--- golang_perfect_developer_guide/workspace/buffer_sample.go
+++ golang_perfect_developer_guide/workspace/buffer_sample.go
@@ -7,16 +7,25 @@ import (
 )
 
 func worker(msg string) <-chan string {
-	limit := make(chan int, 10)
+	// bufferを5に指定
+	limit := make(chan int, 5)
 	receiver := make(chan string)
 	go func() {
 		for i := 0; i < 100; i++ {
+			// NumGoroutineは現在起動しているgoroutineの数を知ることができる
+			// 今回はbufferの数を5としているのでroutineは最大5を期待しているが、
+			// 他のroutineが動いていることもあるので必ずしも上限が5とは限らない
 			log.Println(runtime.NumGoroutine())
-			limit <- 1
+			// limitに送信する
+			// 今回はbufferを5に指定しているので、5回送信がされた時点でchannelがブロックされる
+			// 受信が完了したらまた動かすことができる
+			// 送信: limit <- i
+			// 受信: <-limit
+			limit <- i
 			go func(i int) {
 				msg := fmt.Sprintf("%d %s done", i, msg)
 				receiver <- msg
-				<-limit
+				log.Println("limit :" , <-limit)
 			}(i)
 		}
 	}()
